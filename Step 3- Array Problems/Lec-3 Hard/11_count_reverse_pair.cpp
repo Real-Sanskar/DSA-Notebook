bb97e442b885898b885c27d3a651958eec73fa38
Real-Sanskar <sanskar18016@gmail.com>
2025-12-03 22:50:51 +0530

    Solved : count reverse pair

---
+++ Step 3- Array Problems/Lec-3 Hard/11_count_reverse_pair.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+
+// int reverse_pair(vector<int> &arr){
+//     int n = arr.size();
+
+//     int count = 0;
+//     for(int i=0; i<n; i++){
+//         for(int j=i; j<n; j++){
+//             if(arr[i] > (2 * arr[j])){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+
+
+
+
+int merge(vector<int> &arr, int low, int mid, int high){
+    int left = low;
+    int right = mid+1;
+
+    vector<int> temp;
+    int count = 0;
+
+    while(left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i=low; i<=high; i++){
+        arr[i] = temp[i-low];
+    }
+
+    return count;
+}
+
+int count_pair(vector<int> &arr, int low, int mid, int high){
+    int count = 0, right = mid+1;
+
+    for(int i=low; i<=mid; i++){
+        while(right <= high && arr[i] > 2 * arr[right]){
+            right++;
+        }
+        count += (right - (mid+1));
+    }
+    return count;
+}
+
+int merge_sort(vector<int> &arr, int low, int high){
+    int count = 0;
+    if(low < high){
+        int mid = low + (high-low)/2;
+
+        count += merge_sort(arr,low,mid);
+        count += merge_sort(arr,mid+1,high);
+        count += count_pair(arr,low,mid,high);
+
+        merge(arr,low,mid,high);
+    }
+    return count;
+}
+
+int reverse_pair(vector<int> &arr){
+    int n = arr.size();
+
+    return merge_sort(arr,0,n-1);
+}
+
+int main(){
+    vector<int> arr = {1,3,2,3,1};
+
+    cout << reverse_pair(arr) << endl;
+
+    return 0;
+}
