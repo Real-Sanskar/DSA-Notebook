78cf55b3a286108b1b286b65d6d6d97af3346dfd
Real-Sanskar <sanskar18016@gmail.com>
2025-12-20 23:17:55 +0530

    Solved : Search insertion position

---
+++ Step 4- Binary Search/04_search_insert_position.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int insert_position(vector<int> &arr, int target){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int ans = n;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] >= target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {1,2,4,7};
+    int target = 9;
+
+
+    cout << insert_position(arr,target) << endl;
+
+    return 0;
+}
