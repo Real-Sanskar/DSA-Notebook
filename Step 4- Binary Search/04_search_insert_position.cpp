abddcee2563e8139aa9557c1a9ef10d756306685
Real-Sanskar <sanskar18016@gmail.com>
2025-12-26 23:03:07 +0530

    Update 04_search_insert_position.cpp

--- Step 4- Binary Search/04_search_insert_position.cpp
+++ Step 4- Binary Search/04_search_insert_position.cpp
@@ -1,7 +1,35 @@
+// given an array and a target, 
+// 1. return index if found
+// 2. else return position where it could be inserted
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
+
+// 1. Linear search (TC : O(N)    SC : O(1))
+
+// int insert_position(vector<int> &arr, int target){
+//     int n = arr.size();
+//     int idx = n;
+
+//     for(int i=0; i<n; i++){
+//         if(arr[i] >= target){
+//             return i;
+//         }
+//     }
+//     return idx;
+// }
+
+
+// Binary Search (TC : O(log N)   SC : O(1))
+
+// NOTE - Same as lower bound implementation
+// 1. initialize low and high pointers
+// 2. initialize idx variable with n
+// 3. iterate while(low <= high)
+// 4. if(arr[mid] >= target) -> ans = mid, high = mid + 1
+// 5. else -> low = mid + 1
 
 int insert_position(vector<int> &arr, int target){
     int n = arr.size();
