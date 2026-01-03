c8217fd79e20b1426b35181350efbca9aaa4bd77
Real-Sanskar <sanskar18016@gmail.com>
2026-01-03 22:14:16 +0530

    Update 06_last_occurrence.cpp

--- Step 4- Binary Search/06_last_occurrence.cpp
+++ Step 4- Binary Search/06_last_occurrence.cpp
@@ -10,18 +10,18 @@ using namespace std;
 // 1. Brute force (Linear search (reverse)) (TC : O(N)   SC : O(1))
 
 // traverse from end to start
-// return index when target is found
+// return index when target is found;
 
-int last_occurrence(vector<int> &arr, int target){
-    int n = arr.size();
+// int last_occurrence(vector<int> &arr, int target){
+//     int n = arr.size();
 
-    for(int i=n-1; i>=0; i--){
-        if(arr[i] == target){
-            return i;
-        }
-    }
-    return -1;
-}
+//     for(int i=n-1; i>=0; i--){
+//         if(arr[i] == target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
 
 // Binary Search (TC : O(log N)   SC :O(1))
 
@@ -32,26 +32,26 @@ int last_occurrence(vector<int> &arr, int target){
 // else if(arr[mid] < target) -> search right
 // else -> search left
 
-// int last_occurrence(vector<int> &arr, int target){
-//     int n = arr.size();
-//     int low = 0, high = n-1;
-//     int idx = -1;
+int last_occurrence(vector<int> &arr, int target){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int idx = -1;
 
-//     while(low <= high){
-//         int mid = low + (high - low)/2;
-//         if(arr[mid] == target){
-//             idx = mid;
-//             low = mid + 1;
-//         }
-//         else if(arr[mid] < target){
-//             low = mid + 1;
-//         }
-//         else{
-//             high = mid - 1;
-//         }
-//     }
-//     return idx;
-// }
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] == target){
+            idx = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] < target){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return idx;
+}
 
 int main(){
     vector<int> arr = {3,4,13,13,13,20,40};
