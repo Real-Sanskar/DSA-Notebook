952cd43e396f1bb1fab70893573106b2827ce303
Real-Sanskar <sanskar18016@gmail.com>
2026-04-10 23:12:01 +0530

    Update 03_search_in_2D_matrix_II.cpp

--- Step 4- Binary Search/BS on 2D Arrays/03_search_in_2D_matrix_II.cpp
+++ Step 4- Binary Search/BS on 2D Arrays/03_search_in_2D_matrix_II.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 
+
+// 1. Brute force approach (TC : O(N*M)   SC : O(1))
+
+// Traverse entire matrix
+// If found return true, else return false
+
+
 // bool findElement(vector<vector<int>> &mat, int key){
 //     int n = mat.size();
 //     int m = mat[0].size();
@@ -14,8 +21,73 @@ using namespace std;
 //     return false;
 // }
 
+
+
+
+
+// 2. Better approach (Binary Search)  (TC : O(N + logM)   SC : O(1))
+
+// Find row where target may exists
+// Binary search in that row to find position
+
+
+// bool binarySearch(vector<int> &mat, int key){
+//     int m = mat.size();
+//     int low = 0, high = m-1;
+
+//     while(low<=high){
+//         int mid = low + (high-low)/2;
+
+//         if(mat[mid] == key) return true;
+//         else if(mat[mid] > key) high = mid - 1;
+//         else low = mid + 1;
+//     }
+//     return false;
+// }
+
+// bool findElement(vector<vector<int>> &mat, int key){
+//     int n = mat.size();
+//     int m = mat[0].size();
+
+//     for(int i=0; i<n; i++){
+//         if(mat[i][0] <= key && key <= mat[i][m-1]){
+//             return binarySearch(mat[i],key);
+//         }
+//     }
+//     return false;
+// }
+
+
+
+
+// 3. Optimal approach (TC : O(M+N)   SC : O(1))
+
+// TOP-RIGHT (row=0, col=m-1):
+//   • LARGEST in its ROW (row is sorted left to right)
+//   • SMALLEST in its COLUMN (column is sorted top to bottom)
+
+// BOTTOM-LEFT (row=n-1, col=0):
+//   • SMALLEST in its ROW
+//   • LARGEST in its COLUMN
+
+
 bool findElement(vector<vector<int>> &mat, int key){
     int n = mat.size();
+    int m = mat[0].size();
+    
+    int i = 0, j = m-1;
+    while(i<n && j>=0){
+        if(mat[i][j]==key){
+            return true;
+        }
+        else if(mat[i][j] < key){
+            i++;
+        }
+        else{
+            j--;
+        }
+    }
+    return false;
 }
 
 int main(){
