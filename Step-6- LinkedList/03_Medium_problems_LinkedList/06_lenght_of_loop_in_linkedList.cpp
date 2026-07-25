f3819e1410994be11593cca436c3279723a88b18
Real-Sanskar <sanskar18016@gmail.com>
2026-07-25 23:11:51 +0530

    Optimal Solution: Length of loop

--- Step-6- LinkedList/03_Medium_problems_LinkedList/06_lenght_of_loop_in_linkedList.cpp
+++ Step-6- LinkedList/03_Medium_problems_LinkedList/06_lenght_of_loop_in_linkedList.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// node class for linked list
 class Node{
 public:
     int data;
@@ -19,28 +20,74 @@ public:
 
 class Solution{
 public:
+    // 1. Brute force (Use map to store node and count)  (TC: O(N)  SC:O(N))
     int lengthOfLoop(Node* head){
+        // create map to store visited node and timer value
         unordered_map<Node*, int> visited;
+        // counts number of nodes visited
         int timer = 0;
 
+        // initialize temp node to head
         Node* temp = head;
+        // traverse linked list till temp reaches null ptr
         while(temp != nullptr){
+            // if node already visited, return difference of timer values
             if(visited.find(temp) != visited.end()){
+                // calculate length of loop
                 int loopLength = timer - visited[temp];
                 
                 return loopLength;
             }
-
+            // store the current node and its timer
             visited[temp] = timer;
+            // increament the timer
             timer++;
+            // move to next node
             temp = temp->next;
         }
+        // if traversal is complete, it means no loop detected, return 0
         return 0;
+    }
+
+    // 2. Optimal (Tortoise and hare algorithm)  (TC: O(N)  SC: O(1))
+
+    int lengthOfLoop(Node* head){
+        // initalize the slow and fast pointers to head
+        Node* slow = head;
+        Node* fast = head;
+        
+        // loop until slow and fast meets
+        while(fast != nullptr && fast->next != nullptr){
+            // move slow by one step
+            slow = slow->next;
+            // move fast by 2 steps
+            fast = fast->next->next;
+
+            // if slow and fast meet, loop detected
+            if(slow == fast){
+                // maintain a counter to count nodes traversed in loop
+                int count = 1;
+                fast = fast->next;
+
+                // traverse until they meet again
+                while(fast != slow){
+                    // increase the counter
+                    count++;
+                    fast = fast->next;
+                }
+                // return the length
+                return count;
+            }
+        }
+        // no loop found
+        return 0; 
     }
 };
 
 
+// Driver code
 int main(){
+    // Creating a sample linked list
     Node* head = new Node(1);
     Node* second = new Node(2);
     Node* third = new Node(3);
@@ -52,6 +99,7 @@ int main(){
     third->next = fourth;
     fourth->next = fifth;
 
+    // creating a cycle
     fifth->next = second;
 
     Solution obj;
